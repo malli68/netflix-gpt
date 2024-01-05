9c4cb09c8a087bd0e1103383d65c459dc88e2975
mallikarjun <mallikarjun.vemala@mobileprogramming.com>
2024-01-06 01:57:29 +0530

    logo

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const BANNER =
   "https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 const apikey="8eb0e2749eb9e414e3048e22f949a53f";
 const accesToken="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZWIwZTI3NDllYjllNDE0ZTMwNDhlMjJmOTQ5YTUzZiIsInN1YiI6IjY1OTgyZWI5ZDdhNzBhMTNkMDY5NmQyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7OSwIk6P1N33zxhriHZD5qYVDqHlNW1vYVhMNMahD1o";
-
+export const titleLogo = "https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png";
 export const API_OPTIONS= {
     method: 'GET',
     headers: {
